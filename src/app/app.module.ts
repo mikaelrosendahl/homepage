d06a9d0012735d640ff78d144c84158c7783501e
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-02-26 15:54:37 +0100

    deleted start.comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StartComponent } from './start.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent
+
   ],
   imports: [
     BrowserModule,
