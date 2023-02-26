b2642d9b11deceac20fede7aff159a3838fcdd90
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-02-26 13:33:43 +0100

    added h1 an a comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+
+  /*css selector*/
   selector: 'app-root',
+  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
