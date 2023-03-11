659e608e681a52f4f88c69ab663251bf7bda341f
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-03-11 17:54:46 +0100

    move

--- src/app/start.component.ts
+++ src/app/start.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'app-root',
-  templateUrl: 'start.component.html',
-  styleUrls: ['./css/styling.css']
+
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+
 export class StartComponent {
 
 }
