e88a03a5888e9fe309345cc0239a2088adfbe062
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-03-12 14:11:23 +0100

    small fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
+
 @Component({
 
-  /*css selector*/
   selector: 'app-root',
   templateUrl: './app.component.html',
-
+  
 })
 export class AppComponent {
   title = 'Exploration';
