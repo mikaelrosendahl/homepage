9916bf08ee2cfe9d58978303bffa924ab5c5a851
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-03-12 14:32:54 +0100

    fix on move template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 
 @Component({
-
   selector: 'app-root',
   template: '<app-start></app-start>'
+  
 })
 export class AppComponent {
   title = 'Exploration';
