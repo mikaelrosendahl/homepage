eebd6adf866ef1317fff086424190887cb23aea1
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-04-10 14:08:14 +0200

    added about.css

---
+++ src/app/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+ 
+})
+
+export class StartComponent {
+
+}
