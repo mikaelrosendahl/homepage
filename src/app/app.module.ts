ace0a8e146ff02fe37b3251b312750c3f1131027
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-04-15 14:38:34 +0200

    removed routing app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { StartComponent } from './start.component';
 import { AboutComponent } from './about.component';
@@ -20,7 +20,7 @@ import { ContactComponent } from './contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
