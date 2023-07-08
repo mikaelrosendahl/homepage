9de0e4e0b6dd2a75cd624035883cf32935a69482
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-07-08 22:47:13 +0200

    delete unuseed files and link

--- src/app/login-modal/login-modal.component.ts
+++ src/app/login-modal/login-modal.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
-  styleUrls: ['./login-modal.component.css']
+  
 })
 export class LoginModalComponent {
 
