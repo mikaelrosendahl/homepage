17aaf9cda3722f0b5d00d15adfbe4074a06102c3
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-07-14 16:02:51 +0200

    added function to lang btn

--- src/app/menu.component.ts
+++ src/app/menu.component.ts
@@ -8,9 +8,16 @@ import { TranslateService } from '@ngx-translate/core';
 }) 
 
 export class MenuComponent {
+currentLanguage: string;
+switchLanguage() {
+  const lang = this.currentLanguage === 'en' ? 'sv' : 'en';
+  this.translateService.use(lang);
+  this.currentLanguage = lang;
+}
 
   constructor(private translateService: TranslateService) {
     // Set the default language
+    this.currentLanguage = this.translateService.currentLang;
     this.translateService.setDefaultLang('en');
   }
   showMenu: boolean = true;
