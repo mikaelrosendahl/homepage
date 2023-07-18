3b5b042748d61c0acfc915112b6b871932a327e4
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-07-18 20:35:51 +0200

    fixed hoover color

--- src/app/menu.component.ts
+++ src/app/menu.component.ts
@@ -5,26 +5,25 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 
-}) 
+})
 
 export class MenuComponent {
-currentLanguage: string;
-switchLanguage() {
-  const lang = this.currentLanguage === 'en' ? 'sv' : 'en';
-  this.translateService.use(lang);
-  this.currentLanguage = lang;
-}
+  currentLanguage: string;
+  switchLanguage() {
+    const lang = this.currentLanguage === 'sv' ? 'en' : 'sv';
+    this.translateService.use(lang);
+    this.currentLanguage = lang;
+  }
 
   constructor(private translateService: TranslateService) {
     // Set the default language
     this.currentLanguage = this.translateService.currentLang;
     this.translateService.setDefaultLang('en');
   }
+
   showMenu: boolean = true;
-
-  toogleList(){
-  this.showMenu = !this.showMenu;
-
-   }
+  toogleList() {
+    this.showMenu = !this.showMenu;
+  }
 
 }
