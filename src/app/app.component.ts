0d596912ac1d1cbae6817c26628ec943b00b1a0a
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-07-26 13:59:23 +0200

    added logic and styling to dropmenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exploration';
+
+  showDropdown = false;
+
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
 }
--- src/app/shared-layout/shared-layout.component.ts
+++ src/app/shared-layout/shared-layout.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shared-layout',
   templateUrl: './shared-layout.component.html',
-  styleUrls: ['./shared-layout.component.css']
 })
 export class SharedLayoutComponent {
 
+ 
 }
