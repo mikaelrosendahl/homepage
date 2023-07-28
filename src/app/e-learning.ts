4aeb8e146c7a1cf3563b4876b4526ddddbba3ad5
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-07-28 19:10:44 +0200

    add code

--- src/app/e-learning.ts
+++ src/app/e-learning.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import 'bootstrap';
 @Component({
   selector: 'app-e-learning',
   templateUrl: './e-learning.component.html',
  
 })
 
-export class ElearningComponent {
+export class ElearningComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    // Initialize the Bootstrap carousel after the view has been initialized
+    $('.carousel').carousel();
+  }
 
 }
