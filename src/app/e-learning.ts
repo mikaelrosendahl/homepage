5aa716ee8cc537b5ea8e37510c830d4f6d69471f
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-08-02 11:43:32 +0200

    more styling on e-learn

--- src/app/e-learning.ts
+++ src/app/e-learning.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-e-learning',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class ElearningComponent {
-
+  constructor(private router: Router) {
+    // Set the default language
+  }
+  // Define the changePage() method here
+  changePage(): void {
+    // Use the router to navigate to a particular page
+    this.router.navigate(['/learn']);
+  }
 }
