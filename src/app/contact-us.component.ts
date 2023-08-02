2951a5c4e292e8bf4493758b9e145f340025fab3
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-08-02 22:14:26 +0200

    fixes on contact-form

--- src/app/contact-us.component.ts
+++ src/app/contact-us.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
 })
 
 export class ContactUsComponent {
+  formData = {
 
+    email: '',
+    name: '',
+    message: ''
+  };
+
+  sendMessage() {
+    // Here, you can handle sending the message logic,
+    // such as sending an HTTP request to a backend service.
+    console.log('Message sent:', this.formData);
+  }
 }
