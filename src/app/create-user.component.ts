4058fcf6d7c7b4c8a1d658f6237609bb8e15ace5
mikaelrosendahl <mikaelrosendahl76@gmail.com>
2023-10-14 19:20:06 +0200

    merge master to dev

---
+++ src/app/create-user.component.ts
@@ -0,0 +1,35 @@
+// create-user.component.ts
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+
+})
+export class CreateUserComponent {
+  user: any = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  };
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit(): void {
+    this.http.post<any>('https://localhost:7101/api/CreateUser', this.user).subscribe(
+      response => {
+        console.log('User created successfully!', response);
+        // Reset the form after successful submission
+        this.user = {
+          email: '',
+          password: '',
+          confirmPassword: '',
+        };
+      },
+      error => {
+        console.error('Error creating user:', error);
+      }
+    );
+  }
+}
